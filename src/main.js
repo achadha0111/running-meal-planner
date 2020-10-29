cccb46e8ee2b10c40dba0e93af2a932295157f47
Aayush Chadha <aayush.chadha@student.manchester.ac.uk>
2020-10-29 00:39:28 +0000

    Add routing

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
+import VueMaterial from "vue-material";
 import App from './App.vue'
+import VueRouter from "vue-router";
+import router from "./router";
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(VueRouter);
+Vue.use(VueMaterial);
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+  router,
+  components: { App }
+}).$mount('#app');
