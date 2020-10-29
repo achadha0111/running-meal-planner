11ef7138f90c80535f9f29f5e796a27b860dd189
Aayush Chadha <aayush.chadha@student.manchester.ac.uk>
2020-10-29 09:30:24 +0000

    Add data model

--- src/components/RecipeForm.vue
+++ src/components/RecipeForm.vue
@@ -1,38 +1,40 @@
+<!--TODO Add form validation -->
 <template>
     <div>
-
-        <!-- Search Field -->
         <div class = "md-layout">
             <div class="md-layout-item"></div>
             <div class="md-layout-item">
                 <md-field>
                     <label>Name</label>
-                    <md-input id= "name" placeholder= "Name of recipe"></md-input>
+                    <md-input id= "name" v-model="name" required placeholder= "Name of recipe"></md-input>
+                    <span class="md-error">There is an error</span>
                 </md-field>
                 <md-field>
                     <label for="meal-type">Meal Type</label>
-                    <md-select name="meal-type" id= "meal-type">
+                    <md-select name="meal-type" v-model="mealType" id= "meal-type" required>
                         <md-option value="breakfast">Breakfast</md-option>
                         <md-option value="lunch">Lunch</md-option>
                         <md-option value="dinner">Dinner</md-option>
                     </md-select>
+                    <span class="md-error">There is an error</span>
                 </md-field>
                 <md-field>
                     <label for="workout-type">Workout Type</label>
-                    <md-select name="workout-type" id= "workout-type">
+                    <md-select name="workout-type" v-model="workoutType" id= "workout-type" required>
                         <md-option value="rest">Rest</md-option>
                         <md-option value="normal">Normal</md-option>
                         <md-option value="heavy">Heavy</md-option>
                     </md-select>
+                    <span class="md-error">There is an error</span>
                 </md-field>
                 <md-field>
                     <label>Link</label>
-                    <md-input id= "link" placeholder= "Link (to retrieve ingredients)"></md-input>
+                    <md-input id= "link" v-model="link" placeholder= "Link (to retrieve ingredients)"></md-input>
                 </md-field>
                 <label>Contains Meat?</label>
                 <div>
-                    <md-radio :value="false">True</md-radio>
-                    <md-radio :value="true">False</md-radio>
+                    <md-radio v-model= "containsMeat" :value="false">False</md-radio>
+                    <md-radio v-model="containsMeat" :value="true">True</md-radio>
                 </div>
             </div>
             <div class="md-layout-item"></div>
@@ -54,7 +56,14 @@
 
 <script>
     export default {
-        name: "RecipeForm"
+        name: "RecipeForm",
+        data: () => ({
+            name: '',
+            mealType: '',
+            workoutType: '',
+            link: '',
+            containsMeat: ''
+        }),
     }
 </script>
 
